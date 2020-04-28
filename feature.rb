f4c24f197b5da35534759bc0532b7451fc19a1be
ricardo <ricardotvera.54@gmail.com>
2020-04-28 11:04:45 -0700

    Update feature.rb

--- feature.rb
+++ feature.rb
@@ -1,4 +1,3 @@
-array = [5, 1, 7, 9, 32]
 def bubble_sort(array)
   loop do
     moved = false
@@ -10,24 +9,21 @@ def bubble_sort(array)
     end
     break unless moved
   end
-  return array
 end
   print bubble_sort([5, 1, 7, 9, 32])
-
 
 def bubble_sort_by(array)
   loop do
     moved = false
     (array.length - 1).times do |i|
-      block_comparison = yield(array[i], array[i+1])
-      if block_comparison > 0
+      block_comparison = yield(array[i], array[i + 1])
+      if block_comparison.positive?
         array[i], array[i + 1] = array[i + 1], array[i]
         moved = true
       end
     end
     break unless moved
   end
-  return array
 end
   bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
     left.length - right.length
