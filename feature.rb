ac5ce66a3598d88d4d8cc4afc124a8aa16be2ee9
paulo-techie <paulo.techie@gmail.com>
2020-04-28 11:30:11 -0600

    bubble_sort_by

--- feature.rb
+++ feature.rb
@@ -13,3 +13,22 @@ def bubble_sort(array)
   return array
 end
   print bubble_sort([5, 1, 7, 9, 32])
+
+
+def bubble_sort_by(array)
+  loop do
+    moved = false
+    (array.length - 1).times do |i|
+      block_comparison = yield(array[i], array[i+1])
+      if block_comparison > 0
+        array[i], array[i + 1] = array[i + 1], array[i]
+        moved = true
+      end
+    end
+    break unless moved
+  end
+  return array
+end
+  bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+    left.length - right.length
+  end
